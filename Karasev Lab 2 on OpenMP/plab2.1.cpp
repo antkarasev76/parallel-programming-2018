a5347b28bb5bbc553f633a88b2c8408b995978ae
antkarasev76 <KarasevAnt24@mail.ru>
2019-04-25 21:43:30 +0300

    Corrected version

--- Karasev Lab 2 on OpenMP/plab2.1.cpp
+++ Karasev Lab 2 on OpenMP/plab2.1.cpp
@@ -7,10 +7,12 @@
 #include <random>
 #include <iostream>
 #include <ctime>
+#include <string>
 
 #define Min 200;
 
-int *tmp = nullptr, **offset = nullptr, *vp = nullptr;
+int **offset = nullptr;
+std::vector<int> *tmp;
 
 inline int get_byte(int number, int nbyte)
 {
@@ -27,19 +29,41 @@ void show_mas(int *rand_mas, int length, int i = 0)
 	std::cout << std::endl;
 }
 
-bool valid_of_sort(int *rand_mas, int &length)
+void get_sort_parts(int *rand_mas, int &length, int count_of_threads)
 {
-	bool valid = true;
+	tmp = new std::vector<int>[count_of_threads];
 
-	for (int i = 0; i < length - 1; i++)
+	for (int i = 0; i < count_of_threads; i++)
 	{
-		if (rand_mas[i] > rand_mas[i + 1])
+		for (int j = offset[i][0]; j < offset[i][0] + offset[i][1]; j++)
 		{
-			valid = false;
+			tmp[i].push_back(rand_mas[j]);
+		}
+	}
+}
+
+void valid_of_sort(int *rand_mas, int &length, int idth)
+{
+	int  valid = 0;
+	int i = 0;
+
+	while (tmp[idth].size() != 0)
+	{	
+		int e = rand_mas[i];
+		auto it = std::find(tmp[idth].begin(), tmp[idth].end(), e);
+
+		if (it == tmp[idth].end())
+		{
+			valid = 1;
+			break;
+		}
+		else
+		{
+			tmp[i].erase(it);
 		}
 	}
 
-	return valid;
+	offset[idth][2] = valid;
 }
 
 void counting_sort(int *rand_mas, int *temp, int &length, int &nbyte)
@@ -123,45 +147,6 @@ int BinSearching(int *mas, int l, int r, int x)
 	return BinSearching(mas, l, r, x);
 }
 
-int size_part_tree(int thread_num, int thread_count, int& length)
-{
-	int stock = thread_count, real_count = 0;
-
-	if (thread_num % 2 == 1)
-	{
-		stock = 1;
-	}
-	else if (thread_num % 2 == 0 && thread_num != 0)
-	{
-		int i = 1, k = 1;
-		while (thread_num % (1 << k) == 0)
-		{
-			i *= 2;
-			k++;
-		}
-		stock = i;
-		if (thread_num == thread_count - 1)
-		{
-			stock = 1;
-		}
-	}
-	else
-	{
-		stock = thread_count;
-	}
-
-	if ((thread_num + stock) > thread_count)
-	{
-		stock = thread_count - thread_num;
-	}
-
-	for (int i = thread_num; i < thread_num + stock; ++i)
-	{
-		real_count += offset[i][2];
-	}
-	return real_count;
-}
-
 int* simple_merge(int *mas1, int size_m1, int *mas2, int size_m2)
 {
 	int* res = new int[size_m1 + size_m2];
@@ -202,185 +187,148 @@ int* simple_merge(int *mas1, int size_m1, int *mas2, int size_m2)
 	return res;
 }
 
-void transfer_pair_threads(int own_size, int dest_thread, int size_s, int sch, int count_of_threads, int size_elem, int *rand_mas)
+void transfer_pair_threads2(int *rand_mas, int thread_l, int thread_r)
 {
-	int idth = omp_get_thread_num(), ind1, ind2, elem1, elem2, ind_n = 0;
-	int count = own_size, *res = nullptr;
-
-	/*offset[idth][0] = count;
-
-	int k = 0;
-
-    #pragma omp barrier
-
-    #pragma omp master
+	int idth;
+    #pragma omp parallel shared(rand_mas) private(idth) num_threads(2)
 	{
-		offset[0][1] = offse
-		offset[0][0] = 0;
-		
-		for (int i = 1; i < count_of_threads; i++)
+		idth = omp_get_thread_num();
+		int ind1, ind2, elem1, elem2;
+		int *res = nullptr;
+
+		if (idth == 0)
 		{
-			offset[i][1] = offset[i][0];
-			offset[i][0] = offset[i - 1][0] + offset[i - 1][1];
+			elem1 = rand_mas[(offset[thread_l][0] + offset[thread_l][0] - offset[thread_l][0] / 2 - 1)];
+			ind1 = offset[thread_l][0] + offset[thread_l][0] - offset[thread_l][0] / 2 - 1;
+
+			ind2 = BinSearching(rand_mas, offset[thread_r][0], offset[thread_r][0] + offset[thread_r][1] - 1, elem1);
+
+			while (rand_mas[ind2] >= elem1)
+			{
+				ind2--;
+			}
+			if (ind2 < offset[thread_r][0])
+			{
+				ind2 = offset[thread_r][0] - 1;
+			}
+
+			elem2 = rand_mas[ind2];
+
+			res = simple_merge(rand_mas + offset[thread_l][0], ind1 + 1 - offset[thread_l][0], rand_mas + offset[thread_r][0], ind2 + 1 - offset[thread_r][0]);
+
+			int length = ind1 + 1 - offset[thread_l][0] + ind2 + 1 - offset[thread_r][0], k = 0;
+
+			for (int i = offset[thread_l][0]; i < offset[thread_l][0] + length; i++)
+			{
+				rand_mas[i] = res[k];
+				k++;
+			}
 		}
-	}*/
-
-	//count = offset[idth][0];
-
-	if (sch == 0)
-	{
-		elem1 = rand_mas[(offset[idth][0] + count - count / 2 - 1)];
-		ind1 = offset[idth][0] + count - count / 2 - 1;
-
-		//size_d = (dest_thread == count_of_threads - 1) ? (size_elem - offset[dest_thread][0]) : (offset[dest_thread][0] - offset[dest_thread - 1][0] - 1);
-
-		ind2 = BinSearching(rand_mas, offset[dest_thread][0], offset[dest_thread][0] + size_s - 1, elem1);
-
-		while (rand_mas[ind2] >= elem1)
+		else
 		{
-			ind2--;
+			ind2 = offset[thread_l][1] - offset[thread_l][1] / 2 - 1 + offset[thread_l][0];
+			elem2 = rand_mas[ind2];
+
+			ind1 = BinSearching(rand_mas, offset[thread_r][0], offset[thread_r][0] + offset[thread_r][1] - 1, elem2);
+
+			while (rand_mas[ind1] >= elem2)
+			{
+				ind1--;
+			}
+			if (ind1 < offset[thread_r][0])
+			{
+				ind1 = offset[thread_r][0] - 1;
+			}
+
+			elem1 = rand_mas[ind1];
+
+			res = simple_merge(rand_mas + ind1 + 1, offset[thread_r][1] - (ind1 - offset[thread_r][0] + 1), rand_mas + ind2 + 1, offset[thread_l][1] - (ind2 + 1 - offset[thread_l][0]));
+
+			int length = offset[thread_r][1] - (ind1 - offset[thread_r][0] + 1) + offset[thread_l][1] - (ind2 + 1 - offset[thread_l][0]);
+			int k = 0;
+			for (int i = (ind2 + 1 - offset[thread_l][0] + ind1 + 1 - offset[thread_r][0]); i < ind2 + 1 - offset[thread_l][0] + ind1 + 1 - offset[thread_r][0] + length; i++)
+			{
+				rand_mas[i] = res[k];
+				k++;
+			}
 		}
-		/*if (st_ind > ind2)
-		{
-			ind2 = st_ind;
-		}*/
 
-		elem2 = rand_mas[ind2];
-
-		ind_n = offset[idth][0];
-
-		res = simple_merge(rand_mas + offset[idth][0], ind1 + 1 - offset[idth][0], rand_mas + offset[dest_thread][0], ind2 + 2 - offset[dest_thread][0]);
+		delete res;
 	}
-	else
-	{
-		//ind2 = (offset[dest_thread + 1][0] - offset[dest_thread][0]) - ((offset[dest_thread + 1][0] - offset[dest_thread][0]) / 2) - 1;
-		ind2 = offset[dest_thread][2] - offset[dest_thread][2] / 2 - 1 + offset[dest_thread][2];
-		elem2 = rand_mas[ind2];
-
-		//size_d = (dest_thread == count_of_threads - 1) ? (size_elem - offset[dest_thread][0]) : (offset[dest_thread + 1][0] - offset[dest_thread][0] - 1);
-
-		ind1 = BinSearching(rand_mas, offset[dest_thread][0], offset[dest_thread][0] + offset[dest_thread][2] - 1, elem2);
-
-		while (rand_mas[ind1] >= elem2)
-		{
-			ind1--;
-		}
-		/*if (st_ind > ind1)
-		{
-			ind1 = st_ind;
-		}*/
-
-		elem1 = rand_mas[ind1];
-
-		ind_n = offset[dest_thread][0] + own_size - (ind1 - offset[idth][0] + 1) + offset[idth][0] + offset[idth][2] - ind2 - 1;
-
-		res = simple_merge(rand_mas + offset[idth][0] + ind1 + 1, own_size - (ind1 - offset[idth][0] + 1), rand_mas + offset[dest_thread][0] + ind2 + 1, offset[idth][0] + offset[idth][2] - ind2 - 1);
-	}
-
-    #pragma omp barrier
-
-	if (sch == 0)
-	{
-		int length = ind1 + 1 - offset[idth][0] + ind2 + 2 - offset[dest_thread][0];
-		int k = 0;
-		for (int i = ind_n; i < ind2 + length; i++)
-		{
-			rand_mas[i] = res[k];
-			k++;
-		}
-	}
-	else
-	{
-		int length = own_size - (ind1 - offset[idth][0] + 1) + offset[idth][0] + offset[idth][2] - ind2 - 1;
-		int k = 0;
-		for (int i = ind_n; i < ind2 + length; i++)
-		{
-			rand_mas[i] = res[k];
-			k++;
-		}
-	}
-
-
-	delete res;
-    #pragma omp barrier
 }
 
 void thread_tree(int *rand_mas, int &count_of_threads, int& length)
 {
-	int idth = omp_get_thread_num();
+	int height_tree = 1 + ceil(log2(count_of_threads)), pos = 0;
 
-	int height_tree = 0, cur_count = 0, real_count = 0, size_s = 0, size_d;
-
-	real_count = size_part_tree(idth, count_of_threads, length);
-
-	int dest_thread = idth - (idth % 2);
-	
-	cur_count = offset[idth][2];
-
-	size_s = size_part_tree(dest_thread, count_of_threads, length);
-
-	if (idth != dest_thread)
+	for (int i = 1; i < height_tree; i++)
 	{
-		size_d = size_part_tree(dest_thread, count_of_threads, length);
-		transfer_pair_threads(cur_count, dest_thread, size_s, 1, count_of_threads, length, rand_mas);
-	}
-
-	height_tree = 1 + ceil(log2(count_of_threads));
-
-	for (int i = 1; i < height_tree; ++i)
-	{
-		dest_thread = idth - (idth % (1 << (i + 1)));
-
-		if (idth % (1 << i) == 0)
+		/*for (int j = 0; j < count_of_threads; j += (1 << (i - 1)))
 		{
-			if (i != height_tree - 1)
+			int count = 0;
+
+			transfer_pair_threads2(rand_mas, j, j + (1 << (i - 1)));
+
+			pos = 0;
+			for (int l = 0; l < count_of_threads; l += (1 << (i - 1)))
 			{
-				if ((idth + (1 << (i - 1))) < count_of_threads)
-				{
-					size_s = size_part_tree(idth + (1 << (i - 1)), count_of_threads, length);
-				    transfer_pair_threads(cur_count, idth + (1 << (i - 1)), size_s, 0, count_of_threads, length, rand_mas);
-					cur_count += size_s;
-				}
-
-                #pragma omp barrier
-
-				if (idth != dest_thread)
-				{
-					offset[i][2] = cur_count;
-				}
-
-                #pragma omp master
-				{
-					int pos = 0;
-					for (int j = 0; j < count_of_threads; j += (1 << (i - 1)))
-					{
-						pos += offset[j][2];
-						offset[j + (1 << (i - 1))][0] = pos;
-					}
-				}
-
-				if (idth != dest_thread)
-				{
-					size_s = size_part_tree(idth + (1 << (i - 1)), count_of_threads, length);
-					transfer_pair_threads(cur_count, dest_thread, size_s, 1, count_of_threads, length, rand_mas);
-					cur_count += size_s;
-				}
-
+				pos += offset[l][1];
+				offset[l + (1 << (i - 1))][0] = pos;
 			}
-			else
+		}*/
+
+		int count = 0;
+
+		for (int j = 0; j < count_of_threads; j += (1 << i))
+		{
+			count++;
+		}
+
+		/*int *mas_rangs = new int[count];
+
+		for (int j = 0; j < count; j += (1 << i))
+		{
+			offset[j][2] = j;
+			offset[j + (1 << (i - 1))][2] = j + (1 << (i - 1));
+		}
+
+        #pragma omp parallel shared(rand_mas) num_threads(count)
+		{
+			transfer_pair_threads2(rand_mas, j, j + (1 << (i - 1)));
+		}*/
+
+		if (count > 1)
+		{
+			omp_set_nested(1);
+
+            #pragma omp parallel shared(rand_mas) num_threads(count)
 			{
-				size_s = size_part_tree(idth + (1 << (i - 1)), count_of_threads, length);
-				transfer_pair_threads(cur_count, idth + (1 << (i - 1)), size_s, 0, count_of_threads, length, rand_mas);
-				cur_count += size_s;
+				int idth = omp_get_thread_num(), l = idth;
+				if (idth != 0)
+				{
+					l *= (1 << i);
+				}
+
+				int r = l + (1 << (i - 1));
+
+				transfer_pair_threads2(rand_mas, l, r);
 			}
 		}
+		else
+		{
+			transfer_pair_threads2(rand_mas, 0, 1);
+		}
+
+		//#pragma omp barrier;
+
+		pos = 0;
+		for (int l = 0; l < count_of_threads; l += (1 << (i - 1)))
+		{
+			pos += offset[l][1];
+			offset[l + (1 << (i - 1))][0] = pos;
+		}
+		
 	}
-
-    #pragma omp barrier
-
-	/*vp = rand_mas;
-	rand_mas = tmp;
-	tmp = vp;*/
 }
 
 void parallel_radix_sort(int *rand_mas, int &length, int &count_of_threads)
@@ -389,24 +337,14 @@ void parallel_radix_sort(int *rand_mas, int &length, int &count_of_threads)
     #pragma omp parallel shared(rand_mas) private(i, idth) num_threads(count_of_threads)
 	{
 		int idth = omp_get_thread_num();
-		radix_sort(rand_mas + offset[idth][2], offset[idth][0]);
+		radix_sort(rand_mas + offset[idth][0], offset[idth][1]);
+
+		//valid_of_sort(rand_mas, count_of_elem, idth);
 
         #pragma omp barrier
-
-		/*if (idth == 0)
-		{
-			show_mas(rand_mas, length, 0);
-		}*/
-
-		thread_tree(rand_mas, count_of_threads, length);
-
-        #pragma omp barrier
-
-		/*if (idth == 0)
-		{
-			show_mas(rand_mas, length, 0);
-		}*/
 	}
+
+	thread_tree(rand_mas, count_of_threads, length);
 }
 
 int main(int argc, char* argv[])
@@ -427,7 +365,6 @@ int main(int argc, char* argv[])
 	std::cin >> count_of_elem;
 
 	int *rand_mas = new int[count_of_elem];
-	tmp = new int[count_of_elem];
 	offset = new int*[count_of_threads];
 
 	for (int i = 0; i < count_of_elem; i++)
@@ -444,12 +381,12 @@ int main(int argc, char* argv[])
 	{
 		if (ressidue_c > 0)
 		{
-			offset[i][2] = coeff_prop + 1;
+			offset[i][1] = coeff_prop + 1;
 			--ressidue_c;
 		}
 		else
 		{
-			offset[i][2] = coeff_prop;
+			offset[i][1] = coeff_prop;
 		}
 	}
 
@@ -457,15 +394,13 @@ int main(int argc, char* argv[])
 
 	for (int i = 0; i < count_of_threads - 1; i++)
 	{
-		pos += offset[i][2];
+		pos += offset[i][1];
 		offset[i + 1][0] = pos;
 	}
 
 
 	std::default_random_engine gen(std::time(nullptr));
 	std::uniform_int_distribution<> d(-100, 100);
-
-	//std::cout << "Random massiv: ";
 
 	if (count_of_elem > 2)
 	{
@@ -476,11 +411,6 @@ int main(int argc, char* argv[])
 			    for (i = 0; i < count_of_elem; ++i)
 			    {
 				    rand_mas[i] = d(gen);
-					/*if (omp_get_thread_num() == 0)
-					{
-						std::cout << rand_mas[i] << " ";
-					}*/
-				    
 			    }
 
                 #pragma omp barrier
@@ -491,13 +421,8 @@ int main(int argc, char* argv[])
 		for (i = 0; i < count_of_elem; ++i)
 		{
 			rand_mas[i] = d(gen);
-			//std::cout << rand_mas[i] << " ";
 		}
 	}
-
-	//show_mas(rand_mas, count_of_elem);
-
-	//std::cout << std::endl;
 
 	time1 = omp_get_wtime();
 
@@ -505,26 +430,17 @@ int main(int argc, char* argv[])
 
 	time2 = omp_get_wtime();
 
-	//show_mas(rand_mas, count_of_elem);
-
 	time3 = time2 - time1;
 
-	valid = valid_of_sort(rand_mas, count_of_elem);
+	//get_sort_parts(rand_mas, count_of_elem, 1);
 
-	if (valid == false)
+	//valid_of_sort(rand_mas, count_of_elem, 0);
+
+	/*if (offset[0][2] == 1)
 	{
 		std::cout << "Massiv wasn't sorted" << std::endl;
-	}
+	}*/
 
-	//std::cout << "The operating time of serial algorithm: " << time3 << std::endl;
-
-	//std::cout << "Sorting massiv: ";
-
-	/*for (int i = 0; i < count_of_elem; ++i)
-	{
-		std::cout << rand_mas[i] << " ";
-	}
-	std::cout << std::endl;*/
 
 	if (count_of_elem > 2)
 	{
@@ -535,7 +451,6 @@ int main(int argc, char* argv[])
 			for (i = 0; i < count_of_elem; ++i)
 			{
 				rand_mas[i] = d(gen);
-				//std::cout << rand_mas[i] << " ";
 			}
 
             #pragma omp barrier
@@ -546,11 +461,10 @@ int main(int argc, char* argv[])
 		for (i = 0; i < count_of_elem; ++i)
 		{
 			rand_mas[i] = d(gen);
-			//std::cout << rand_mas[i] << " ";
 		}
 	}
 
-	//show_mas(rand_mas, count_of_elem);
+	//get_sort_parts(rand_mas, count_of_elem, 1);
 
 	time1 = omp_get_wtime();
 
@@ -560,20 +474,25 @@ int main(int argc, char* argv[])
 
 	time4 = time2 - time1;
 
-	//show_mas(rand_mas, count_of_elem);
+	/*bool valid = true;
 
-
-	valid = valid_of_sort(rand_mas, count_of_elem);
-
-	if (valid == false)
+	for (int i = 0; i < count_of_threads; i++)
 	{
-		std::cout << "Massiv wasn't sorted" << std::endl;
-	}
+		if (offset[i][2] == 1)
+		{
+			valid = false;
+		}
+	}*/
+
+	/*if (valid == false)
+	{
+	std::cout << "Massiv wasn't sorted" << std::endl;
+	}*/
 
 	std::cout << "The operating time of serial algorithm: " << time3 << std::endl;
 	std::cout << "The operating time of parallel algorithm: " << time4 << std::endl;
 
-	//free(tmp);
+	system("pause");
 
 	return 0;
 }
